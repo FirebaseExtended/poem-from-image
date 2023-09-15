b003305ac4578d246b1d1d12173384da663afec5
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2023-09-15 16:43:35 -0400

    lint

--- src/ImageGrid.tsx
+++ src/ImageGrid.tsx
@@ -1,25 +1,18 @@
+import { User } from "firebase/auth";
 import { collection, query, where } from "firebase/firestore";
+import { ref as createStorageRef } from "firebase/storage";
 import { Link } from "react-router-dom";
 import {
-  ref as createStorageRef,
-  uploadBytesResumable,
-  UploadTask,
-  UploadTaskSnapshot,
-  StorageReference,
-} from "firebase/storage";
-import {
-    useAuth,
+  useAuth,
   useFirestore,
   useFirestoreCollectionData,
   useStorage,
   useStorageDownloadURL,
-  useUser,
 } from "reactfire";
 import { Skeleton } from "./components/ui/skeleton";
-import { User } from "firebase/auth";
 
 function PlaceholderImage() {
-    return <Skeleton className="w-32 h-32 rounded-sm"/>;
+  return <Skeleton className="w-32 h-32 rounded-sm" />;
 }
 
 function PoemImage({ imagePath }: { imagePath: string }) {
@@ -29,32 +22,45 @@ function PoemImage({ imagePath }: { imagePath: string }) {
   );
 
   if (status === "loading") {
-    return <PlaceholderImage/>;
+    return <PlaceholderImage />;
   }
 
-  return <img className="rounded-sm transition ease-in-out hover:drop-shadow-lg hover:scale-105 " src={imageURL} />;
+  return (
+    <img
+      className="rounded-sm transition ease-in-out hover:drop-shadow-lg hover:scale-105 "
+      src={imageURL}
+    />
+  );
 }
 export default function ImageGrid() {
   const firestore = useFirestore();
   const poemsCollection = collection(firestore, "poems");
   const auth = useAuth();
-  const poemsQuery = query(poemsCollection, where('user', "==", (auth.currentUser as User).uid))
+  const poemsQuery = query(
+    poemsCollection,
+    where("user", "==", (auth.currentUser as User).uid)
+  );
 
   const { status, data } = useFirestoreCollectionData(poemsQuery, {
     idField: "poemId",
   });
 
   return (
-    <><div className="p-1 flex justify-center"><h1>Other poems you've made</h1></div>
-    <div className="p-2 grid grid-cols-2 gap-2 justify-items-center align-middle">
-        
-      {status === "loading" ? [1,2,3,4,5].map((x) => <PlaceholderImage key={x}/>): data.map((poem) => {
-        return (
-          <Link key={poem.poemId} to={`poems/${poem.poemId}`}>
-            <PoemImage imagePath={poem.image} />
-          </Link>
-        );
-      })}
-    </div></>
+    <>
+      <div className="p-1 flex justify-center">
+        <h1>Other poems you've made</h1>
+      </div>
+      <div className="p-2 grid grid-cols-2 gap-2 justify-items-center align-middle">
+        {status === "loading"
+          ? [1, 2, 3, 4, 5].map((x) => <PlaceholderImage key={x} />)
+          : data.map((poem) => {
+              return (
+                <Link key={poem.poemId} to={`poems/${poem.poemId}`}>
+                  <PoemImage imagePath={poem.image} />
+                </Link>
+              );
+            })}
+      </div>
+    </>
   );
 }
