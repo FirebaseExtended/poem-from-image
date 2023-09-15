3fe9f5889a6289bd35d031fcc7fe5e2f42170511
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2023-09-15 16:42:43 -0400

    colors

--- src/Root.tsx
+++ src/Root.tsx
@@ -21,17 +21,17 @@ function ProfilePic() {
 
 function Root() {
   return (
-    <div className="bg-sky-200 h-screen grid grid-cols-6 grid-rows-6 gap-4 p-4">
-      <div className="rounded-sm row-span-1 col-span-6 bg-sky-300 p-6  flex justify-between items-center">
+    <div className="bg-slate-200 h-screen grid grid-cols-6 grid-rows-6 gap-4 p-4 text-slate-700">
+      <div className="rounded-sm row-span-1 col-span-6 bg-slate-50 p-6  flex justify-between items-center">
         <Link to="/">
-          <h1 className="text-4xl">Poems from pictures</h1>
+          <h1 className="font-extrabold text-4xl text-slate-900">Poems from pictures</h1>
         </Link>
         <ProfilePic />
       </div>
-      <div className="rounded-sm drop-shadow bg-sky-100 col-span-4 row-span-5  grow flex flex-col items-center justify-center p-5">
+      <div className="rounded-sm drop-shadow bg-slate-50 col-span-4 row-span-5  grow flex flex-col items-center justify-center p-5">
         <Outlet />
       </div>
-      <div className="rounded-sm bg-sky-100 col-span-2 row-span-5  overflow-x-hidden overflow-y-auto">
+      <div className="rounded-sm bg-slate-50 col-span-2 row-span-5  overflow-x-hidden overflow-y-auto">
         <ImageGrid />
       </div>
     </div>
