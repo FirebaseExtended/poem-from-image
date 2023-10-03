368cc8a2430cf9acb296282aadd181285f4cbaa0
Chris Gill <chrisraygill@gmail.com>
2023-10-03 22:07:18 +0000

    code updated

--- functions/main.py
+++ functions/main.py
@@ -1,13 +1,8 @@
-# Import the Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 from firebase_functions import firestore_fn
-
-# Import the Firebase Admin SDK to access Cloud Firestore and Cloud Storage.
 from firebase_admin import initialize_app, firestore, storage
-
-# Import the Vertex AI client library to access caption and text generation models.
 import vertexai
 from vertexai.language_models import TextGenerationModel
-from vertexai.vision_models import ImageCaptioningModel
+from vertexai.vision_models import ImageCaptioningModel, Image
 
 # Initialize app
 app = initialize_app()
@@ -20,16 +15,13 @@ vertexai.init(project=PROJECT_ID, location=REGION)
 # Listen to the poems Firestore collection for document creation
 @firestore_fn.on_document_created(document="poems/{poemId}")
 def generate_poem_from_image(event: firestore_fn.Event[firestore_fn.DocumentSnapshot]):
-  doc = event.data
-  doc_ref = doc.reference
+  doc_ref = event.data.reference
+  image_path = event.data.get("image")
+  bucket = storage.bucket("poem-from-image.appspot.com")
 
   # Download the image as bytes and create a vertex image
-  image_path = doc.get("image")
-  bucket = storage.bucket("poem-from-image.appspot.com")
   image_bytes = bucket.blob(image_path).download_as_bytes()
-  vertex_image = vertexai.vision_models.Image(image_bytes)
-  doc_ref.update(
-    {"status": "IMAGE_DOWNLOADED"})
+  vertex_image = Image(image_bytes)
 
   # Generate captions from the image bytes and update status
   caption_model = ImageCaptioningModel.from_pretrained("imagetext@001")
@@ -43,7 +35,6 @@ def generate_poem_from_image(event: firestore_fn.Event[firestore_fn.DocumentSnap
 
   # Generate a poem from the image captions and update status
   text_model = TextGenerationModel.from_pretrained("text-bison")
-  doc_ref.update({"status": "GENERATING_POEM"})
   poem = text_model.predict(
       prompt=f"Write a poem about the following image caption: {captions[0]}",
       temperature=0.9,
